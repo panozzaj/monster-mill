ee3cd934108382b0090d0cae1b949d9cddcbef61
Anthony Panozzo <panozzaj@gmail.com>
2015-12-12 17:54:54 -0500

    Start extracting some structs

--- src/main.cpp
+++ src/main.cpp
@@ -23,14 +23,28 @@ enum Button {
     NONE,
 };
 
+struct Monster {
+    Monster* nextMonster;
+
+    // space
+    int position;
+
+    // status
+    int hunger;
+};
+
+struct Pen {
+    Monster *firstMonster;
+    int treats[WRITABLE_WIDTH];
+};
+
+Pen pen;
+
 // define some values used by the panel and buttons
 Button last_button = NONE;
 int last_button_pressed_at;
-int adc_key_in = 0;
 int cursor_position = 0;
-int treats[WRITABLE_WIDTH];
-int current_pen = 1;
-int monster_position = 0;
+int current_pen_index = 1;
 
 // symbols must be positive integers or cast as byte
 #define SYMBOL_CARROT 1
@@ -70,13 +84,15 @@ void setup() {
 
     // set up initial program state
     for (int i = 0; i < WRITABLE_WIDTH; ++i) {
-        treats[i] = 0;
+        pen.treats[i] = 0;
+        pen.firstMonster = (Monster*)malloc(sizeof(Monster));
+        pen.firstMonster->position = 0;
     }
 }
 
 // read the buttons
 Button readButtons() {
-    adc_key_in = analogRead(0);      // read the value from the sensor
+    int adc_key_in = analogRead(0);      // read the value from the sensor
 
     // LCD buttons when read are centered at values:
     //  Right: 0
@@ -89,26 +105,29 @@ Button readButtons() {
 
     // check none first since it will be the most likely result
     if (adc_key_in >= 790) return NONE;
-    if (adc_key_in < 50)   return RIGHT;
-    if (adc_key_in < 195)  return UP;
-    if (adc_key_in < 380)  return DOWN;
-    if (adc_key_in < 555)  return LEFT;
+    if (adc_key_in <   50) return RIGHT;
+    if (adc_key_in <  195) return UP;
+    if (adc_key_in <  380) return DOWN;
+    if (adc_key_in <  555) return LEFT;
     return SELECT; // >= 555, < 790
 }
 
-void printMonsters(int pen) {
-  lcd.setCursor(0, TOP_LINE);
-  char the_character[1];
-  itoa(pen, the_character, 10);
-  lcd.print(the_character);
+void printMonsters() {
+    lcd.setCursor(0, TOP_LINE);
+    char the_character[1];
+    itoa(current_pen_index, the_character, 10);
+    lcd.print(the_character);
 
-  for (int position = 0; position < WRITABLE_WIDTH; position++) {
-      if (monster_position == position) {
-          lcd.print("a");
-      } else {
-          lcd.print(" ");
-      }
-  }
+    Monster* monster = pen.firstMonster;
+
+    String positions = "               ";
+
+    while (monster) {
+        positions[monster->position] = 'a';
+        monster = monster->nextMonster;
+    }
+
+    lcd.print(positions);
 }
 
 void printCursor() {
@@ -121,11 +140,12 @@ void printCursor() {
     }
 }
 
-void printBottomLine(int pen) {
+void printBottomLine() {
     lcd.setCursor(0, BOTTOM_LINE);
     lcd.write(SYMBOL_CARROT);
+
     for (int i = 0; i < WRITABLE_WIDTH; ++i) {
-        if (treats[i] == 1) {
+        if (pen.treats[i] == 1) {
             lcd.write(SYMBOL_CARROT);
         } else {
             lcd.print(" ");
@@ -136,7 +156,13 @@ void printBottomLine(int pen) {
 }
 
 void updateMonsters() {
-    monster_position = (millis() / 1000 % WRITABLE_WIDTH);
+    int new_position;
+    Monster *monster = pen.firstMonster;
+
+    while (monster) {
+        monster->position = millis() / 1000 % WRITABLE_WIDTH;
+        monster = monster->nextMonster;
+    }
 }
 
 void processInput() {
@@ -148,10 +174,10 @@ void processInput() {
         } else if (last_button == RIGHT) {
             cursor_position = cursor_position + 1;
         } else if (last_button == SELECT) {
-            if (treats[cursor_position] == 0) {
-                treats[cursor_position] = 1;
+            if (pen.treats[cursor_position] == 0) {
+                pen.treats[cursor_position] = 1;
             } else {
-                treats[cursor_position] = 0;
+                pen.treats[cursor_position] = 0;
             }
         }
         // make sure we are on the writable board
@@ -162,8 +188,8 @@ void processInput() {
 }
 
 void loop() {
-    printMonsters(current_pen);
-    printBottomLine(current_pen);
     processInput();
+    printMonsters();
+    printBottomLine();
     updateMonsters();
 }
