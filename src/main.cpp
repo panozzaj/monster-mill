dc68e0d36794f010496c64a53d42d647b8da8f59
Anthony Panozzo <panozzaj@gmail.com>
2015-12-12 15:55:03 -0500

    Draw cursor on bottom of screen

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,26 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-// define some values used by the panel and buttons
-int lcd_key = 0;
-int adc_key_in = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
+#define TOP_LINE 0
+#define BOTTOM_LINE 1
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
 
-// symbols must be positive integers
+// define some values used by the panel and buttons
+int last_key = 0;
+int adc_key_in = 0;
+int cursor_position = 0;
+
+#define BUTTON_RIGHT  0
+#define BUTTON_UP     1
+#define BUTTON_DOWN   2
+#define BUTTON_LEFT   3
+#define BUTTON_SELECT 4
+#define BUTTON_NONE   5
+
+// symbols must be positive integers or cast as byte
 #define SYMBOL_CARROT 1
+#define SYMBOL_UNDERSCORE 2
 
 byte symbol_carrot[8] = {
     B00000,
@@ -31,14 +39,28 @@ byte symbol_carrot[8] = {
     B00000,
 };
 
+byte symbol_underscore[8] = {
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B11111,
+};
+
 int monster_position = 0;
 
 void setup() {
     Serial.begin(9600);
 
     // initialize LCD screen
-    lcd.begin(16, 2);
+    lcd.begin(LCD_WIDTH, LCD_HEIGHT);
+
+    // create custom characters
     lcd.createChar(SYMBOL_CARROT, symbol_carrot);
+    lcd.createChar(SYMBOL_UNDERSCORE, symbol_underscore);
 }
 
 // read the buttons
@@ -46,24 +68,28 @@ int read_lcd_buttons() {
     adc_key_in = analogRead(0);      // read the value from the sensor
 
     // LCD buttons when read are centered at values:
-    //   0, 144, 329, 504, 741
+    //  Right: 0
+    //  Up: 144
+    //  Down: 329
+    //  Left: 504
+    //  Select: 741
     //
     // Add ~50 to those values since they are analog
 
     // check none first since it will be the most likely result
-    if (adc_key_in >= 790) return btnNONE;
-    if (adc_key_in < 50)   return btnRIGHT;
-    if (adc_key_in < 195)  return btnUP;
-    if (adc_key_in < 380)  return btnDOWN;
-    if (adc_key_in < 555)  return btnLEFT;
-    return btnSELECT; // >= 555, < 790
+    if (adc_key_in >= 790) return BUTTON_NONE;
+    if (adc_key_in < 50)   return BUTTON_RIGHT;
+    if (adc_key_in < 195)  return BUTTON_UP;
+    if (adc_key_in < 380)  return BUTTON_DOWN;
+    if (adc_key_in < 555)  return BUTTON_LEFT;
+    return BUTTON_SELECT; // >= 555, < 790
 }
 
 void printMonsters(int pen) {
-  lcd.setCursor(0, 0);
-  char theCharacter[1];
-  itoa(pen, theCharacter, 10);
-  lcd.print(theCharacter);
+  lcd.setCursor(0, TOP_LINE);
+  char the_character[1];
+  itoa(pen, the_character, 10);
+  lcd.print(the_character);
 
   for (int position = 0; position < 15; position++) {
       if (monster_position == position) {
@@ -74,12 +100,28 @@ void printMonsters(int pen) {
   }
 }
 
+void printCursor() {
+    lcd.setCursor(1, BOTTOM_LINE);
+
+    if (millis() / 500 % 2 == 0) {
+        lcd.print(" ");
+    } else {
+        lcd.write(SYMBOL_UNDERSCORE);
+    }
+}
+
+void printBottomLine(int pen) {
+    lcd.setCursor(0, BOTTOM_LINE);
+    lcd.write(SYMBOL_CARROT);
+    printCursor();
+}
+
+void updateMonsters() {
+    monster_position = (millis() / 1000 % 15);
+}
+
 void loop() {
     printMonsters(1);
-
-    lcd.setCursor(0, 1);
-    lcd.write(SYMBOL_CARROT);
-    lcd_key = read_lcd_buttons();
-
-    monster_position = (millis() / 500 % 15);
+    printBottomLine(1);
+    updateMonsters();
 }
